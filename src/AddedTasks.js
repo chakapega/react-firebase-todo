e677d5ea2f697416785ea6615e05150a8e0e0c8a
Vadim <chakapega@gmail.com>
2019-06-22 14:12:52 +0300

    added key to every task

--- src/AddedTasks.js
+++ src/AddedTasks.js
@@ -6,10 +6,11 @@ import editPng from './images/edit.png'
 export default class AddedTasks extends Component {
   render() {
     let tasks = this.props.tasks;
+    let taskIndex = 0;
 
     let tasksElements = tasks.map(task => {
       return (
-        <div className="added-task_container">
+        <div className="added-task_container" key={task.name + taskIndex++}>
         <span className="added-task_span">{task.name} | {task.description}</span>
         <div className="image-container">
           <img className="img-edit" title="Edit" src={editPng} alt="edit"/>
