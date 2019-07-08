948f3a280d3372a2ae0914ab49ba371010ee09fb
Vadim <chakapega@gmail.com>
2019-07-08 23:09:17 +0300

    Proper implementation of the AddedTasks component

--- src/AddedTasks.js
+++ src/AddedTasks.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
-import './AddedTasks.css';
-import deletePng from './images/delete.png';
-import editPng from './images/edit.png';
+import Tasks from './Tasks';
 
 export default class AddedTasks extends Component {
   render() {
     const { tasks, removeTask, showTaskForm } = this.props;
     
-    const tasksElements = tasks.map(task => {
-    const { id, name, description } = task;
-
-      return (
-        <div className="added-task_container" key={id} id={id}>
-        <span className="added-task_span">{name} | {description}</span>
-        <div className="image-container">
-          <img className="img-edit" title="Edit" src={editPng} alt="edit" onClick={showTaskForm}/>
-          <img className="img-delete" title="Remove" src={deletePng} alt="delete" onClick={removeTask}/>
-        </div>
-        </div>
-      );
-    });
-    
     return (
-      <div>
-        {tasksElements}
-      </div>
+      <Tasks tasks={tasks} removeTask={removeTask} showTaskForm={showTaskForm}/>
     );
   };
 };
